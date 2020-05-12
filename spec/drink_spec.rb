17d0c2726105c33c928f15e828f9b90603e398ad
yodev <taboo.hansel@gmail.com>
2020-05-12 18:43:47 +0900

    Add drinkクラスのテストコードを記載

--- spec/drink_spec.rb
+++ spec/drink_spec.rb
@@ -1,13 +1,25 @@
 require 'spec_helper'
 describe Drink do
-  # describe "自動販売機" do
-  #   context "コーラを入れた場合" do
-  #     it "コーラが出力されること" do
-  #       drink = Drink.cola
-  #       expect(drink.output).to eq 'cola : 120'
-  #     end
-  #     it "モンスターが出力されること" do
-  #     end
-  #   end
-  # end
+  
+  describe "アクセサメソッド" do
+    context "読み込み処理行った場合" do
+      it "コーラの情報が表示されること" do
+        cola = Drink.cola
+        expect(cola.name).to eq(:cola)
+        expect(cola.price).to eq(120)
+      end
+
+      it "レッドブルの情報がされること" do
+        redbull = Drink.redbull
+        expect(redbull.name).to eq(:redbull)
+        expect(redbull.price).to eq(200)
+      end
+
+      it "水の情報が表示されること" do
+        water = Drink.water
+        expect(water.name).to eq(:water)
+        expect(water.price).to eq(100)
+      end
+    end
+  end
 end
